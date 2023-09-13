faf96f3c5f3708db1f7729bdcd9eb3ec43baf6eb
tigran <tigransuqiasyan@gmail.com>
2023-09-12 23:31:17 -0700

    solution for all counts

---
+++ 2801. Count Stepping Numbers in Range/countForAllLengths.java
@@ -0,0 +1,41 @@
+class Solution {
+    private static int[][] nextInts = {{1},{2,0},{3,1},{4,2},{5,3},{6,4},{7,5},{8,6},{9,7},{8}};
+    private static int mod = 7+1000000000;
+
+    public int countSteppingNumbers(String low, String high) {
+        int total = 0;
+        for(int len = low.length() ; len <= high.length() ;++len){
+            total += (getCount( len) % mod);
+            total %= mod;
+        }
+        return total;
+        
+    }
+    private  int getCount( int length){
+        int count = 0;
+        List<Integer> currentPath = new LinkedList<>();
+        for(int i=1;i<10;++i) {
+            currentPath.add(i);
+            count += (getCount(length-1, currentPath) % mod);
+            count %= mod;
+            currentPath.remove(0);
+        }
+        return count;
+    }
+    private  int getCount(int length, List<Integer> path){
+        if(length == 0){
+            return 1;
+        }
+        int last = path.get(path.size()-1);
+        int count =0;
+        for(int i=0;i<nextInts[last].length;++i){
+            
+            path.add(nextInts[last][i]);
+            count += (getCount(length-1, path)%mod);
+            count %= mod;
+            path.remove(path.size()-1);
+        }
+       
+        return count;
+    }
+}
